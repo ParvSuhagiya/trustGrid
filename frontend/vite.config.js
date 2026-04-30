adada43993dea68538f29d3c7cd76cea5f177316
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2026-04-30 15:16:24 +0530

    changed link of backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://trustgrid.onrender.com',
         changeOrigin: true,
+        secure: true,
       },
     },
   },
